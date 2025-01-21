d00a58f1b80fc6be0322b51695f0b27f2027cbf9
ryone9re <ryoichi.kurimoto@gmail.com>
2025-01-21 03:32:44 +0000

    Clear

--- length.ml
+++
@@ -1,8 +0,0 @@
-let rec length list =
-  match list with
-  [] -> 0
-  | first :: rest -> 1 + length rest
-
-let test1 = length [] = 0
-let test2 = length [1] = 1
-let test3 = length [1; 2] = 2
--- tsurukame.ml
+++
@@ -1,9 +0,0 @@
-(* 鶴と亀の合計の頭数と足の数から鶴の頭数を計算する *)
-(* int -> int -> int *)
-let tsurukame h f = (4 * h - f) / 2
-
-let test1 = tsurukame 0 0 = 0
-let test2 = tsurukame 1 2 = 1
-let test3 = tsurukame 1 4 = 0
-let test4 = tsurukame 2 4 = 2
-let test5 = tsurukame 3 8 = 2
